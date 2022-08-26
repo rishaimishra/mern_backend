f4486bcb2f36fd22e536a44d768463e2832d5702
srvtechnology <srvtechnology0@gmail.com>
2022-08-26 15:59:40 +0530

    image uploading

--- routers/products.js
+++ routers/products.js
@@ -3,6 +3,21 @@ const { Category } = require('../models/category');
 const router = express.Router();
 const {Product} = require('../models/product')
 const mongoose = require('mongoose')
+const multer = require('multer')
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null,path.join(__dirname, '../public/uploads/'))
+    },
+    filename: function (req, file, cb) {
+    //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        const fileName = file.originalname.split(' ').join('-') 
+      cb(null, fileName + '-' + Date.now())
+    }
+  })
+  
+  const uploadOptions = multer({ storage: storage })
 
 router.get(`/`, async (req, res) => {
     // localhost:3000/api/v1/products?categories=2122,2422
@@ -40,18 +55,20 @@ router.get('/:id', async (req, res) =>{
     return res.status(200).json({product})
 })
 
-router.post(`/`, async (req, res) => {
+router.post(`/`,uploadOptions.single('image'), async (req, res) => {
 
     try {
 
         let category = await Category.findById(req.body.category);
     if(!category) return res.status(400).send('Invalid Category');
-
+        const fileName = req.file.filename
+        const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
+        console.log(basePath);
     let product = new Product({
         name: req.body.name,
         description: req.body.description,
         richDescription: req.body.richDescription,
-        image: req.body.image,
+        image: `${basePath}${fileName}`,
         brand: req.body.brand,
         price: req.body.price,
         category: req.body.category,
